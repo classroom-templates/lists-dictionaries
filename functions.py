1e92459935da8f381bd08f5e98e4e2888540de25
Alexander Katrompas <alexander.katrompas@austincc.edu>
2021-03-27 14:03:22 -0500

    initial commit

---
+++ functions.py
@@ -0,0 +1,42 @@
+
+# write a function called planets_list()
+# pass it the planets dictionary and status list
+# return the list of planets that are true planets
+# base on the status list
+#
+# you may NOT use python methods; min, max, sum, or mean
+# or any "magic" functions. you must use a loop to do
+# this manually
+
+
+
+
+# write a function called furthest()
+# pass it the planets dictionary and an OPTIONAL boolean flag
+# the flag decides if you use the CLOSEST distance or the FURTHEST
+# distance to decide which planet is furthest
+# in other words, use index 0 or 1 from the distance lists
+# return the name of the furthest planet
+#
+# you may not use python methods; min, max, sum, or mean
+# or any "magic" functions. you must use a loop to do
+# this manually
+
+
+
+
+
+# write a function called closest()
+# pass it the planets dictionary and an OPTIONAL boolean flag
+# the flag decides if you use the CLOSEST distance or the FURTHEST
+# distance to decide which planet is closest
+# in other words, use index 0 or 1 from the distance lists
+# return the name of the closest planet
+#
+# you may not use python methods; min, max, sum, or mean
+# or any "magic" functions. you must use a loop to do
+# this manually
+
+
+
+
---
+++ main.py
@@ -0,0 +1,26 @@
+#
+# your comment header here
+#
+
+import functions as fn
+
+def main():
+    # do not modify this function except while coding to help you figure
+    # out the functions you are writing. before you submit, main() must be
+    # returned to exactly this state or there may be severe grade penalty.
+    
+    sol = {"Uranus":[2750, 3000, 2880], "Mercury":[46, 70, 57], "Earth":[147, 152, 150], "Venus":[107, 109, 108], "Mars":[205, 249, 228], "Saturn":[1350, 1510, 1430], "Jupiter":[741, 817, 779], "Pluto":[4440, 7380, 5910], "Neptune":[4450, 4550, 4500]}
+    status = [True, True, True, True, True, True, True, False, True]
+    # note that the "arrays" above are what are known as "parallel arrays"
+    # this means that have values that relate to each other. The first "True"
+    # in status relates to "Uranus" in sol. The second "True" to "Mercury", etc.
+    
+    print()
+    print("The closest by nearest distance is:", closest(sol))
+    print("The closest by furthest distance is:", closest(sol, False))
+    print("The furthest by nearest distance is:", furthest(sol, False))
+    print("The furthest by furthest distance is:", furthest(sol))
+    print("The list of all planets is:", planets_list(sol, status))
+    print()
+    
+main()
