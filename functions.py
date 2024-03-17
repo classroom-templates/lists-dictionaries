6226d53732fd3eeeb40a4e18b02f21fa4987504b
Alexander Katrompas <alexander.katrompas@austincc.edu>
2024-03-17 13:10:29 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
-#
+# ########################
 # Your comment header here
-#
+# #########################
 
 
 # constants needed for controlling find_nearest() and find_furthest()
@@ -13,33 +13,56 @@ AVG = 2
 # along with a flag to determine which distance to use
 # defaulting to average if no flag is passed
 # return the name of the nearest planet
+# ERASE THIS INSTRUCTIONAL COMMENT WHEN YOU ARE DONE!
 def find_nearest():
-    pass
+    """
+    put your correct function docstring here, according to
+    https://katrompas.accprofessors.com/commenting
+    """
+    pass # erase this instruction when you are done
 
 # pass in a dictionary of planets and distances
 # along with a flag to determine which distance to use
 # defaulting to average if no flag is passed
 # return the name of the furthest planet
+# ERASE THIS INSTRUCTIONAL COMMENT WHEN YOU ARE DONE!
 def find_furthest():
-    pass
-
+    """
+    put your correct function docstring here, according to
+    https://katrompas.accprofessors.com/commenting
+    """
+    pass # erase this instruction when you are done
 
 # pass in a dictionary of planets and distances
 # along with a flag to determine which metric to display, kilometers or miles
 # defaulting to kilometers if no flag value is passed
 # print out the planets and their distances
+# ERASE THIS INSTRUCTIONAL COMMENT WHEN YOU ARE DONE!
 def display_planet_distances():
-    pass
+    """
+    put your correct function docstring here, according to
+    https://katrompas.accprofessors.com/commenting
+    """
+    pass # erase this instruction when you are done
 
 # pass in a dictionary of planets and distances
 # along with a flag to determine alphabetical order
 # defaulting to unsorted if no flag value is passed
 # return a list of planet names sorted or unsorted
+# ERASE THIS INSTRUCTIONAL COMMENT WHEN YOU ARE DONE!
 def get_planets():
-    pass
+    """
+    put your correct function docstring here, according to
+    https://katrompas.accprofessors.com/commenting
+    """
+    pass # erase this instruction when you are done
 
 # pass in a list of planet names
 # print the list of planets as shown in the examples
+# ERASE THIS INSTRUCTIONAL COMMENT WHEN YOU ARE DONE!
 def list_planets():
-    pass
-
+    """
+    put your correct function docstring here, according to
+    https://katrompas.accprofessors.com/commenting
+    """
+    pass # erase this instruction when you are done
